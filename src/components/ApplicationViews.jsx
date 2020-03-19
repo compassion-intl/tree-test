1840277638ef548ac844c0b070b45d4bb96e42c3
Colin Sandlin <csandlin@us.ci.org>
2020-03-19 14:44:33 -0600

    cleaning up

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -1,11 +1,11 @@
 import { Route } from "react-router-dom";
 import React from "react";
 import Home from "./home/Home";
-import Task1 from "../components/tests/Task1";
-import Task2 from "../components/tests/Task2";
-import Task3 from"../components/tests/Task3";
-import Task4 from "../components/tests/Task4";
- 
+import Task1 from "./tasks/Task1";
+import Task2 from "./tasks/Task2";
+import Task3 from "./tasks/Task3";
+import Task4 from "./tasks/Task4";
+
 const ApplicationViews = () => {
   return (
     <>
@@ -34,11 +34,14 @@ const ApplicationViews = () => {
           return <Task3 />;
         }}
       />
-      <Route path="/task4">
-        <Task4 />
-      </Route>
+      <Route
+        path="/task4"
+        render={props => {
+          return <Task4 />;
+        }}
+      ></Route>
     </>
   );
 };
- 
+
 export default ApplicationViews;
---
+++ src/components/tasks/Task4.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NavItems from "../../data/info2";
+import SideNav from "../sidenav/SideNav";
+import "../sidenav/SideNav.css";
+
+const Task4 = () => {
+  return <SideNav items={NavItems} />;
+};
+
+export default Task4;
